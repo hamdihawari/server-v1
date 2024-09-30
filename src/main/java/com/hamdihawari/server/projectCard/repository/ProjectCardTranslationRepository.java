bf9662d17f7e57743cc53db28a8f7f52abda49bb
hamdihawari <hamdi.hawari@outlook.com>
2024-10-01 00:51:46 +0200

    HH-69 <projectCard, projectCardTranslation CRUD>

---
+++ src/main/java/com/hamdihawari/server/projectCard/repository/ProjectCardTranslationRepository.java
@@ -0,0 +1,6 @@
+package com.hamdihawari.server.projectCard.repository;
+
+import com.hamdihawari.server.projectCard.entity.ProjectCardTranslation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectCardTranslationRepository extends JpaRepository<ProjectCardTranslation, Long> {}
